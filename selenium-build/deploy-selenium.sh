c9290ce187cbc566e11b4f875b7ceb79ef2ee731
gorvin <igor.n.vincic@gmail.com>
2017-04-27 16:02:32 +0200

    Comments and script for selenium custom build and deployment

---
+++ selenium-build/deploy-selenium.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+pushd m2local/org/seleniumhq/selenium
+
+VERSION="$1"
+GROUPID="org.seleniumhq.selenium"
+REPO_URL=https://artifactory.propelmedia.com/artifactory/libs-release-local
+DEPLOY_ARGS="deploy:deploy-file -DrepositoryId=afrepo-release -Durl=${REPO_URL} -DgroupId=${GROUPID} -Dversion=${VERSION}"
+
+function deploy() {
+    local artifactId=$1
+    local FILEBASE=${artifactId}/${VERSION}/${artifactId}-${VERSION}
+    ls ${FILEBASE}.jar ${FILEBASE}.pom
+    mvn ${DEPLOY_ARGS} -DartifactId=${artifactId} -Dpackaging=jar -Dfile=${FILEBASE}.jar -DpomFile=${FILEBASE}.pom
+    mvn ${DEPLOY_ARGS} -DartifactId=${artifactId} -Dpackaging=jar -Dclassifier=javadoc -Dfile=${FILEBASE}-javadoc.jar -DpomFile=${FILEBASE}-javadoc.pom
+    mvn ${DEPLOY_ARGS} -DartifactId=${artifactId} -Dpackaging=jar -Dclassifier=sources -Dfile=${FILEBASE}-sources.jar -DpomFile=${FILEBASE}-sources.pom
+}
+
+deploy lift
+deploy selenium-api
+deploy selenium-chrome-driver
+deploy selenium-edge-driver
+deploy selenium-firefox-driver
+deploy selenium-ie-driver
+deploy selenium-java
+deploy selenium-leg-rc
+deploy selenium-opera-driver
+deploy selenium-remote-driver
+deploy selenium-safari-driver
+deploy selenium-server
+deploy selenium-support
+
+popd
