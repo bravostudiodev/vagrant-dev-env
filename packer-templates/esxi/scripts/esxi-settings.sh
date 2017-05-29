5c0bce6ee0220f016a7c6e8a730e54a60f518330
gorvin <igor.n.vincic@gmail.com>
2017-05-29 15:03:42 +0200

    Pack esxi box as root but let vagrant provision as user vagrant and authorize insecure key for user vagrant.

--- packer-templates/esxi/scripts/esxi-settings.sh
+++ packer-templates/esxi/scripts/esxi-settings.sh
@@ -1,2 +1,29 @@
+set -ex
+
 esxcli network firewall ruleset set -e true -r httpClient
 esxcli system settings advanced set -o /Net/GuestIPHack -i 1
+
+# https://pubs.vmware.com/vsphere-60/index.jsp?topic=%2Fcom.vmware.vcli.examples.doc%2Fcli_manage_users.9.5.html
+
+sed -e 's/^\(password\s*requisite\s*.*\)$/#\1/' \
+    -e 's/^\(password\s*required\s*.*\)$/#\1/' \
+    -e 's/^\(password\s*sufficient\s*.*\) use_authtok \(.*\)$/\1 \2/' \
+    -i /etc/pam.d/passwd
+
+esxcli system account add -i vagrant -p vagrant -c vagrant
+
+sed -e 's/^#\(password\s*requisite\s*.*\)$/\1/' \
+    -e 's/^#\(password\s*required\s*.*\)$/\1/' \
+    -e 's/^\(password\s*sufficient\s*.*pam_unix.so\) \(.*\)$/\1 use_authtok \2/' \
+    -i /etc/pam.d/passwd
+
+# http://kblnrz.blogspot.ba/2016/11/vcap-dcv-addedit-remove-users-on-esxi_21.html
+esxcli system permission set -i vagrant -r Admin
+
+# Remove unsupported SSHD option
+sed -e 's/^\(PrintLastLog .*\)$/#\1/' -i /etc/ssh/sshd_config
+
+# Authorize vagrant insecure key
+mkdir -p /etc/ssh/keys-vagrant
+cp /etc/ssh/keys-root/authorized_keys /etc/ssh/keys-vagrant/authorized_keys
+chown vagrant:vagrant -R /etc/ssh/keys-vagrant
